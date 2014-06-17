0937c52e1c1cb9ec092fccdcb836c5cf90808bbd
armanc <armands.baris@gmail.com>
2014-06-18 01:26:47 +0300

    Quick training with Angular UI Router

--- js/js.js
+++ js/js.js
@@ -1,4 +1,4 @@
-var app = angular.module('myApp', ['ui.bootstrap']);
+var app = angular.module('myApp', ['ui.bootstrap', 'ui.router']);
 
 //app.config(function($routeProvider) {
 //    $routeProvider
@@ -41,3 +41,33 @@ app.controller("writeBgColor", function ($scope) {
 
 });
 
+app.config(function($stateProvider, $urlRouterProvider) {
+  //
+  // For any unmatched url, redirect to /state1
+  $urlRouterProvider.otherwise("/state1");
+  //
+  // Now set up the states
+  $stateProvider
+    .state('state1', {
+      url: "/state1",
+      templateUrl: "parts/state1.html"
+    })
+    .state('state1.list', {
+      url: "/list",
+      templateUrl: "parts/state1.list.html",
+      controller: function($scope) {
+        $scope.items = ["A", "List", "Of", "Items"];
+      }
+    })
+    .state('state2', {
+      url: "/state2",
+      templateUrl: "parts/state2.html"
+    })
+    .state('state2.list', {
+      url: "/list",
+        templateUrl: "parts/state2.list.html",
+        controller: function($scope) {
+          $scope.things = ["A", "Set", "Of", "Things"];
+        }
+      })
+    });
